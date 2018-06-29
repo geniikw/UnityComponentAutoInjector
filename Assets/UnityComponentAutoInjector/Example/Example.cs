2a46266f5c892c0b143ad23118ad8629b49fbb9a
Hyunsoo Kim <hsk@calmid.com>
2018-06-29 15:19:15 +0900

    Added Example.

---
+++ Assets/UnityComponentAutoInjector/Example/Example.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    [SerializeField, GetComponentInParent]
+    private Camera parent = null;
+
+    [SerializeField, GetComponentInChildren]
+    private Camera children = null;
+
+    [SerializeField, GetComponentInChildrenOnly(false)]
+    private Camera childrenOnly = null;
+
+    [SerializeField, GetComponent]
+    private Camera example = null;
+
+    [SerializeField, FindGameObject("Directional Light")]
+    private Camera find = null;
+}
